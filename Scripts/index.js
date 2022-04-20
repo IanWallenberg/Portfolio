b49cfb6e974725e9ca300f6f13ff6058ec930580
Ian Wallenberg <ianwallenberg@Ians-MBP.localdomain>
2022-04-20 15:49:21 +0200

    Adding images, styling, profile section

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,18 +1,27 @@
 
-console.log("Before timeout")
+// console.log("Before timeout")
 
-const timeoutPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("set to three seconds inside promise")
-    resolve("The value of the promise");
-  }, 3 * 1000);
-});
+// const timeoutPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("set to three seconds inside promise")
+//     resolve("The value of the promise");
+//   }, 3 * 1000);
+// });
 
-timeoutPromise.then((result) => {
-  console.log("timeout is done!", result);
-})
-.catch((error) => {
-  console.log("TimeoutPromise errored out", error)
-});
+// timeoutPromise.then((result) => {
+//   console.log("timeout is done!", result);
+// })
+// .catch((error) => {
+//   console.log("TimeoutPromise errored out", error)
+// });
 
-console.log("after set timeout");
+// console.log("after set timeout");
+
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
