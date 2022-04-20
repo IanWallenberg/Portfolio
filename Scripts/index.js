183fefab5bfc25e505f58ce9914d36072bd5e44a
Ian Wallenberg <ianwallenberg@Ians-MBP.localdomain>
2022-04-20 16:14:15 +0200

    adding slider functionality, example pictures for show

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,27 +1,27 @@
 
-// console.log("Before timeout")
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// const timeoutPromise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     console.log("set to three seconds inside promise")
-//     resolve("The value of the promise");
-//   }, 3 * 1000);
-// });
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-// timeoutPromise.then((result) => {
-//   console.log("timeout is done!", result);
-// })
-// .catch((error) => {
-//   console.log("TimeoutPromise errored out", error)
-// });
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// console.log("after set timeout");
-
-// function myFunction() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
