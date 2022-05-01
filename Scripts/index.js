37c5c960080b2e4bac13c3baae9a5c6904f8a2c1
Ian Wallenberg <ianwallenberg@ians-mbp.home>
2022-05-01 13:34:32 +0200

    added mobile layout for start page and profile

--- Scripts/index.js
+++ Scripts/index.js
@@ -26,14 +26,14 @@
 //   dots[slideIndex-1].className += " active";
 // }
 
-let imgBx = document.querySelectorAll('.imgBx');
-		imgBx.forEach(popup => popup.addEventListener('click', () =>{
-			popup.classList.toggle('active')
-		}));
+// let imgBx = document.querySelectorAll('.imgBx');
+// 		imgBx.forEach(popup => popup.addEventListener('click', () =>{
+// 			popup.classList.toggle('active')
+// 		}));
 
-		function toggleMenu(){
-	      var menuToggle = document.querySelector('.toggle');
-	      var navigation = document.querySelector('.navigation');
-	      menuToggle.classList.toggle('active');
-	      navigation.classList.toggle('active');
-	    }
+// 		function toggleMenu(){
+// 	      var menuToggle = document.querySelector('.toggle');
+// 	      var navigation = document.querySelector('.navigation');
+// 	      menuToggle.classList.toggle('active');
+// 	      navigation.classList.toggle('active');
+// 	    }
