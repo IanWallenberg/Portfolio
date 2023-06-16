9a3162043cf5947e23fced37408498f73c94b0e6
Ian <ian_wallenberg@hotmail.se>
2023-06-16 10:51:44 +0200

    Updating w React
    Adding React to project (separate works)

---
+++ my-portfolio/src/data/user.js
@@ -0,0 +1,86 @@
+const INFO = {
+  main: {
+    title: "Portfolio by Ian Wallenberg",
+    name: "Ian Wallenberg",
+    email: "ian_wallenberg@hotmail.se",
+    logo: "../logo.png",
+  },
+
+  socials: {
+    twitter: "https://twitter.com/",
+    github: "https://github.com/",
+    linkedin: "https://linkedin.com/",
+    instagram: "https://instagram.com/",
+    stackoverflow: "https://stackoverflow.com/",
+    facebook: "https://facebook.com/",
+  },
+
+  homepage: {
+    title: "Hello there. Welcome to my portfolio!",
+    description:
+      " When it comes to coding, I enjoy working with React a lot. It is such a fun framework to work with, and also a challenge to work with in order to understand Javascript better. I'd love to make contacts and get to know people studying Front End, or get to know people already in the business, or just get to know driven and passionate people who want to learn and share their experiences of life. Send a message if you're curious, or just add me on LinkedIn, GitHub, Instagram or Facebook (see links below). I'm open for anything, and looking forward to new connections! I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+  },
+
+  about: {
+    title:
+      "I'm Ian Wallenberg, a video game nerd, Japan enthusiast, and board game lover with a passion for music, movies, and arts. I like to try out a lot of new things, experience the world and share good vibes.",
+    description:
+      "I have studied the Front End Developer Program at Hyper Island in Stockholm from 2021-2023. I've worked on a variety of projects during my studies and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+  },
+
+  articles: {
+    title:
+      "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+    description:
+      "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+  },
+
+  projects: [
+    {
+      title: "Project 1",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 2",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 3",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 4",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 5",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+  ],
+};
+
+export default INFO;
