d9ef9c3e9f2b70caf4636a4db7fec876b66fa33b
Ian <ian_wallenberg@hotmail.se>
2023-06-21 12:44:33 +0200

    Rearranging icons and pics
    Changing icons, pics, text, cleaning up article section

--- my-portfolio/src/data/user.js
+++ my-portfolio/src/data/user.js
@@ -7,11 +7,9 @@ const INFO = {
   },
 
   socials: {
-    twitter: "https://twitter.com/",
     github: "https://github.com/",
     linkedin: "https://linkedin.com/",
     instagram: "https://instagram.com/",
-    stackoverflow: "https://stackoverflow.com/",
     facebook: "https://facebook.com/",
   },
 
@@ -23,59 +21,84 @@ const INFO = {
 
   about: {
     title:
-      "I'm Ian Wallenberg, a video game nerd, Japan enthusiast, and board game lover with a passion for music, movies, and arts. I like to try out a lot of new things, experience the world and share good vibes.",
+      "I'm Ian Wallenberg, a video game nerd, Japan enthusiast, and board game lover with a passion for music, movies, and arts.",
     description:
       "I have studied the Front End Developer Program at Hyper Island in Stockholm from 2021-2023. I've worked on a variety of projects during my studies and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
   },
 
-  articles: {
-    title:
-      "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-    description:
-      "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-  },
-
   projects: [
     {
-      title: "Project 1",
+      title: "Project 1 - Website",
       description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+        "First project at Hyper Island, a website made with basic HTML, CSS, and JavaScript for my friend and coworker Bedros Kabranien. Having a client in mind I tried to create an easy website to showcase his qualities",
+      logoHTML:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      logoCSS:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
       logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
       linkText: "View Project",
-      link: "https://github.com",
+      link: "https://github.com/IanWallenberg/Project-Bedros",
     },
 
     {
-      title: "Project 2",
+      title: "Project 2 - Countdown timer",
       description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
-      linkText: "View Project",
-      link: "https://github.com",
-    },
-
-    {
-      title: "Project 3",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-      linkText: "View Project",
-      link: "https://github.com",
-    },
-
-    {
-      title: "Project 4",
-      description:
-        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+        "Second project at Hyper Island, a timer with information stored in local storage. The first group project where we learned about functions in JavaScript more in-depth, as well as working with Agile methodologies and Trello",
+      logoHTML:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      logoCSS:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
       logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
       linkText: "View Project",
-      link: "https://github.com",
+      link: "https://github.com/IanWallenberg/IASS-Project",
+    },
+
+    {
+      title: "Project 3 - Quiz",
+      description:
+        "Third project at Hyper Island, multiple quizzes and storing data with Firebase. The second group project where we learned more about API's, JavaScript functions, teamwork, and using Git and Github",
+      logoHTML:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      logoCSS:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com/IanWallenberg/IASS-Quiz-Project-1",
+    },
+
+    {
+      title: "Project 4 - Video game",
+      description:
+        "Fourth project at Hyper Island, creating a video game in Phaser.js. The third group project where we learned how to work with the framework Phaser.js, diving into video game logic and finishing off with a video game exhibition for all groups to showcase their finished product",
+      logoHTML:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      logoCSS:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com/IanWallenberg/moderate-younger-brother-giovani-game",
     },
 
     {
       title: "Project 5",
       description:
         "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logoHTML:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      logoCSS:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
+      logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+    {
+      title: "Project 6",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logoHTML:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      logoCSS:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
       logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
       linkText: "View Project",
       link: "https://github.com",
